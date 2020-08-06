c19a182e1bc9f084d1411df2e7eda161b2b44104
drogba <drogba02@naver.com>
2020-08-06 20:26:24 +0900

    자료형 테스트

--- Partice_Java/src/test/test.java
+++ Partice_Java/src/test/test.java
@@ -2,6 +2,14 @@ package test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        double dd = 2.544;
+        int aa = (int) dd;
+        System.out.println(dd);
+        System.out.println(aa);
+
+        int a = 32;
+        double d = a;
+        System.out.println(a);
+        System.out.println(d);
     }
 }
