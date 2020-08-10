87094b66871b67317926870568194f5254f27b1e
drogba <drogba02@naver.com>
2020-08-10 19:33:53 +0900

    자바 오버로딩 , 생성자 스터디

--- Partice_Java/src/ch2/CharToCode.java
+++ Partice_Java/src/ch2/CharToCode.java
@@ -1,2 +1,14 @@
-package ch2;public class CharToCode {
+package ch2;
+
+public class CharToCode {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int code = (int)ch;
+
+        System.out.printf("%c=%d(%#X) %n" , ch , code , code);
+
+        System.out.printf("%#X %n" , 65);
+        System.out.printf("%#X %n" , 66);
+        System.out.printf("%#X %n" , 67);
+    }
 }
