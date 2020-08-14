6671baa2ad44518b3d53bea5816dd9822388467c
drogba <drogba02@naver.com>
2020-08-14 18:25:22 +0900

    매개변수의 다형성

--- Partice_Java/src/ch07/PolyArgumentTest.java
+++ Partice_Java/src/ch07/PolyArgumentTest.java
@@ -1,2 +1,61 @@
-package ch07;public class PolyArgumentTest {
+package ch07;
+
+class Product {
+    int price;
+    int bonusPoint;
+
+    Product(int price) {
+        this.price = price;
+        this.bonusPoint = (int) (price/10.0);
+//        bonusPoint = (int) (price/10.0);
+    }
+}
+
+class PTv extends Product {
+    PTv() {
+        super(100);
+    }
+
+    public String toString() {
+        return "PTv";
+    }
+}
+
+class PComputer extends Product {
+    PComputer() {
+        super(200);
+    }
+
+    public String toString() {
+        return "PComputer";
+    }
+}
+
+class Buyer {
+    int money = 100;
+    int bonusPoint = 0;
+
+    void buy(Product p) {
+        if (money < p.price) {
+            System.out.println("잔액 부족");
+            return;
+        }
+
+        money -= p.price;
+        bonusPoint += p.bonusPoint;
+        System.out.println(p + "구입!");
+    }
+}
+
+public class PolyArgumentTest {
+    public static void main(String[] args) {
+        Buyer b = new Buyer();
+
+        b.buy(new PTv());
+        b.buy(new PComputer());
+
+        System.out.println("남은돈:" + b.money);
+        System.out.println("보너스포인트:" + b.bonusPoint);
+
+    }
 }
--- Partice_Java/src/ch07/PolyArgumentTest2.java
+++ Partice_Java/src/ch07/PolyArgumentTest2.java
@@ -1,2 +1,84 @@
-package ch07;public class PolyArgumentTest2 {
+package ch07;
+
+class Product2 {
+    int price;
+    int bonusPoint;
+
+    Product2(int price) {
+        this.price = price;
+        this.bonusPoint = (int) (price/10.0);
+    }
+}
+
+class Tv2 extends Product2 {
+    Tv2() {
+        super(100);
+    }
+
+    public String toString() {
+        return "Tv";
+    }
+}
+
+class Computer2 extends Product2 {
+    Computer2() {
+        super(200);
+    }
+
+    public String toString() {
+        return "Computer";
+    }
+}
+
+class Audio2 extends Product2 {
+    Audio2() {
+        super(50);
+    }
+
+    public String toString() {
+        return "Audio";
+    }
+}
+
+class Buyer2 {
+    int money = 1000;
+    int bonusPoint = 0;
+    Product2[] item = new Product2[10];
+    int i = 0;
+
+    void buy(Product2 p) {
+        if (money < p.price) {
+            System.out.println("잔액 부족");
+            return;
+        }
+
+        money -= p.price;
+        bonusPoint += p.bonusPoint;
+        item[i++] = p;
+        System.out.println(p + "구입");
+    }
+
+    void summary() {
+        int sum = 0;
+        String itemList = "";
+
+        for (int i = 0; i < item.length; i++) {
+            if (item[i] == null) break;
+            sum += item[i].price;
+            itemList += item[i] + ",";
+        }
+
+        System.out.println("총 금액:" + sum);
+        System.out.println("제품들:" + itemList);
+    }
+}
+
+public class PolyArgumentTest2 {
+    public static void main(String[] args) {
+        Buyer2 b = new Buyer2();
+        b.buy(new Tv2());
+        b.buy(new Computer2());
+        b.buy(new Audio2());
+        b.summary();
+    }
 }
