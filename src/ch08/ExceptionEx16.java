3112e9ce78175f95b6faaf1af4b082fa023072b7
limwoobin <drogba02@naver.com>
2020-08-17 15:51:44 +0900

    자바 예외처리 스터디 #2

---
+++ src/ch08/ExceptionEx16.java
@@ -0,0 +1,23 @@
+package ch08;
+
+import java.io.File;
+
+public class ExceptionEx16 {
+    public static void main(String[] args) {
+        try {
+            File f = createFile(args[0]);
+            System.out.println(f.getName() + "파일 생성");
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + "다시 입력해주세요.");
+        }
+    }
+
+    static File createFile(String fileName) throws Exception {
+        if (fileName == null || fileName.equals("")) {
+            throw new Exception("파일이름이 유효하지 않음.");
+        }
+        File f = new File(fileName);
+        f.createNewFile();
+        return f;
+    }
+}
--- src/test/test.java
+++ src/test/test.java
@@ -14,6 +14,16 @@ public class test {
         Object o1 = new B();
 //        B b1 = (B) new A(); 런타임 오류
         B b2 = (B) a1;
+
+        String[] arr = {"A" , "B" , "C"};
+        print(arr);
+    }
+
+    static void print(String... arr) {
+        System.out.println(arr.length);
+        for (String msg : arr) {
+            System.out.println(msg);
+        }
     }
 }
 
