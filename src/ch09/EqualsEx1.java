e67302f7fe214f67e0d589fd1c3a6ee7ab8e45ea
limwoobin <drogba02@naver.com>
2020-08-17 18:31:04 +0900

    java.lang 패키지 #1

---
+++ src/ch09/EqualsEx1.java
@@ -0,0 +1,29 @@
+package ch09;
+
+public class EqualsEx1 {
+    public static void main(String[] args) {
+        Value v1 = new Value(10);
+        Value v2 = new Value(10);
+
+        if (v1.equals(v2)) {
+            System.out.println("v1 과 v2 는 같음");
+        } else {
+            System.out.println("v1 과 v2 는 다름");
+        }
+
+        v2 = v1;
+        if (v1.equals(v2)) {
+            System.out.println("v1 과 v2 는 같음");
+        } else {
+            System.out.println("v1 과 v2 는 다름");
+        }
+    }
+}
+
+class Value {
+    int value;
+
+    Value(int value) {
+        this.value = value;
+    }
+}
