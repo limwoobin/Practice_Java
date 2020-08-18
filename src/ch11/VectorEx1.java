fec9a097427d946d331f390b8b74447e6f4bd80d
limwoobin <drogba02@naver.com>
2020-08-18 18:55:08 +0900

    자바 컬렉션프레임워크(List , Stack , Queue) 스터디

---
+++ src/ch11/VectorEx1.java
@@ -0,0 +1,37 @@
+package ch11;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorEx1 {
+    public static void main(String[] args) {
+        List list = null;
+        Vector v = new Vector(5);
+        v.add("1");
+        v.add("2");
+        v.add("3");
+        print(v);
+
+        v.trimToSize();
+        System.out.println("==After trimToSize() ===");
+        print(v);
+
+        v.ensureCapacity(6);
+        System.out.println("==After ensureCapacity==");
+        print(v);
+
+        v.setSize(7);
+        System.out.println("==After setSize(7)==");
+        print(v);
+
+        v.clear();
+        System.out.println("==After clear==");
+        print(v);
+    }
+
+    static void print(Vector v) {
+        System.out.println(v);
+        System.out.println("size:" + v.size());
+        System.out.println("capacity:" + v.capacity());
+    }
+}
