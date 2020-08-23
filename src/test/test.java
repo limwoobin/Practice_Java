20dc0ba261636ad0d7b02f8c4dc893411061b1a7
limwoobin <drogba02@naver.com>
2020-08-23 20:57:56 +0900

    자바 Enum 스터디

--- src/test/test.java
+++ src/test/test.java
@@ -5,20 +5,24 @@ import java.util.Hashtable;
 
 public class test {
     public static void main(String[] args) {
-        String a = "935a2cdd-3d5c-4fc2-a7b0-18e462f5239f";
-        String b = "bde14643-6ce2-469c-93ff-1d1d948ed85c";
-        System.out.println(a.length());
-        System.out.println(b.length());
+        System.out.println(Kind.CLOVER);
+        System.out.println(Kind.CLOVER. name());
+        System.out.println(Kind.CLOVER.getText());
 
-        // 헤더토큰값 36
-        Hashtable table = new Hashtable();
-        table.put("a" , 1);
-        table.put("b" , 2);
+    }
+}
 
-        HashMap map = new HashMap();
-        map.put("A" , 1);
-        map.put("B" , 2);
-        System.out.println(table);
-        System.out.println(map);
+enum Kind {
+
+    CLOVER("CLOVER");
+
+    private String text;
+
+    Kind(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
     }
 }
