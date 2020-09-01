b2b994b3fe8bc276c70aca19eb2a978e16d6b401
drogba <drogba02@naver.com>
2020-09-01 20:09:57 +0900

    자바 Stream 스터디

---
+++ src/ch14/StreamEx1.java
@@ -0,0 +1,52 @@
+package ch14;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StreamEx1 {
+    public static void main(String[] args) {
+        Stream<Student> studentStream = Stream.of(
+                new Student("이자바" , 3 , 300),
+                new Student("김자바" , 1 , 200),
+                new Student("박자바" , 2 , 100),
+                new Student("안자바" , 2 , 150),
+                new Student("노자바" , 1 , 200),
+                new Student("백자바" , 3 , 200),
+                new Student("표자바" , 3 , 180)
+        );
+
+        studentStream.sorted(Comparator.comparing(Student::getBan)
+                                        .thenComparing(Comparator.naturalOrder()))
+                                        .forEach(System.out::println);
+    }
+}
+
+class Student implements Comparable<Student> {
+    String name;
+    int ban;
+    int totalScore;
+    Student(String name , int ban , int totalScore) {
+        this.name = name;
+        this.ban = ban;
+        this.totalScore = totalScore;
+    }
+
+    public String toString() {
+        return String.format("[%s , %d , %d" , name , ban , totalScore).toString();
+    }
+
+    String getName() {
+        return name;
+    }
+    int getBan() {
+        return ban;
+    }
+    int getTotalScore() {
+        return totalScore;
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        return s.totalScore - this.totalScore;
+    }
+}
