21ce62bc8ce3f867edad76a5378a6b7a83029c27
limwoobin <drogba02@naver.com>
2020-09-05 16:41:37 +0900

    자바 데이터 기반 스트림

---
+++ src/ch15/DataInputStreamEx3.java
@@ -0,0 +1,27 @@
+package ch15;
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class DataInputStreamEx3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int score = 0;
+
+        try (FileInputStream fis = new FileInputStream("score.dat");
+             DataInputStream dis = new DataInputStream(fis)) {
+
+             while (true) {
+                 score = dis.readInt();
+                 System.out.println(score);
+                 sum += score;
+             }
+        } catch (EOFException e) {
+            System.out.println("총합:" + sum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
