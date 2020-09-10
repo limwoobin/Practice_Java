f2e192eeeabbc5d03d0209c78c9ab0c7a362759c
limwoobin <drogba02@naver.com>
2020-09-10 19:55:25 +0900

    자바 가변인자 오버로딩

---
+++ src/remind/VarArgsEx.java
@@ -0,0 +1,25 @@
+package remind;
+
+public class VarArgsEx {
+    public static void main(String[] args) {
+        String[] strArr = {"100" , "200" , "300"};
+
+        System.out.println(concatenate("" , "100" , "200" , "300"));
+        System.out.println(concatenate("A" , strArr));
+    }
+
+    static String concatenate(String delim , String... args) {
+        String result = "";
+
+        for (String str : args) {
+            result += str + delim;
+        }
+
+        return result;
+    }
+
+//    static String concatenate(String... args) {
+//
+//        return concatenate("" , args);
+//    }
+}
