1dd679bb35cd94a952614e6572eaa3642760a5cb
limwoobin <drogba02@naver.com>
2020-10-09 15:58:09 +0900

    Java8 Lambda express
    LambdaEx

---
+++ src/java8/lambda/ex3/LambdaEx1.java
@@ -0,0 +1,35 @@
+package java8.lambda.ex3;
+
+@FunctionalInterface
+interface MyFunction {
+    void run();
+}
+
+public class LambdaEx1 {
+    static void execute(MyFunction f) {
+        f.run();
+    }
+
+    static MyFunction getMyFunction() {
+        MyFunction f = () -> System.out.println("f3 Run !!");
+        return f;
+    }
+
+    public static void main(String[] args) {
+        MyFunction f1 = () -> System.out.println("f1 Run !!");
+        MyFunction f2 = new MyFunction() {
+            @Override
+            public void run() {
+                System.out.println("f2 Run !!");
+            }
+        };
+        MyFunction f3 = getMyFunction();
+
+        f1.run();
+        f2.run();
+        f3.run();
+
+        execute(f1);
+        execute(() -> System.out.println("run"));
+    }
+}
