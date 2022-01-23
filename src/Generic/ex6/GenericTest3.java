4377ee5c7618b76568add316f6e70b2f8ed60a87
limwoobin <drogba02@naver.com>
2022-01-23 19:53:41 +0900

    Generic
    wildcard , type #3

--- src/Generic/ex6/GenericTest3.java
+++ src/Generic/ex6/GenericTest3.java
@@ -9,13 +9,24 @@ public class GenericTest3 {
         integerList.add(3);
         integerList.add(2);
         integerList.add(1);
-
         listPrint(integerList);
+
+        List<Long> longList = new ArrayList<>();
+        longList.add(1L);
+        longList.add(2L);
+        longList.add(3L);
+        listPrint2(longList);
     }
 
     static void listPrint(List<?> list) {
         for (Object t : list) {
             System.out.println(t);
+        }
+    }
+
+    static void listPrint2(List<? extends Number> list) {
+        for (Number n : list) {
+            System.out.println(n);
         }
     }
 }
